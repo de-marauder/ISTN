b1f4552388a9bc5a88a1fc49aeede291f8f8005f
coderdivi <khosee6@gmail.com>
2023-01-04 07:43:47 +0100

    added responsiveness

---
+++ stuff.js
@@ -0,0 +1,19 @@
+
+document.querySelector('.menu-btn').addEventListener('click', () => document.querySelector('.main-menu').classList.toggle('show'))
+function animate() {
+    var reveals = document.querySelectorAll(".animate");
+  
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 150;
+  
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
+      }
+    }
+  }
+  
+  window.addEventListener("scroll", animate);
