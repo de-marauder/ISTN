a833e546e1d21ef3c241f124f372a08bbf4be9d8
coderdivi <khosee6@gmail.com>
2023-01-06 08:06:39 +0100

    updated images

--- MAIn CEREAL landing page/file.js
+++ MAIn CEREAL landing page/file.js
@@ -18,19 +18,5 @@ function animate() {
   
   window.addEventListener("scroll", animate);
 
-  let slideIndex =0;
-  showSlides();
 
-  function showSlides()  {
-    let i;
-    let slides=document.getElementsByClassName(".mySLides");
-    for (i=0; i<slides.length;i++){
-      slides[i].getElementsByClassName.display="none"
-    }
-    slideindex++;
-    if(slideIndex > slides.length){
-      slideIndex=1
-    }
-    slides[slideImdex-1].getElementsByClassName.display="block";
-    setTimeout(showSlides, 2000)
-  }
+  
